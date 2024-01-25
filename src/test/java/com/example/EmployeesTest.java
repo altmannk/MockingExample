a0e9621bfaa58316841f51d41753d61be5708e64
altmannk <katherine.altmann@iths.se>
2024-01-25 13:50:08 +0100

    Add second test to EmployeesTest with Mockito

--- src/test/java/com/example/EmployeesTest.java
+++ src/test/java/com/example/EmployeesTest.java
@@ -2,9 +2,11 @@ package com.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class EmployeesTest {
 
@@ -20,4 +22,22 @@ class EmployeesTest {
         assertThat(payments).isEqualTo(3);
     }
 
+    @Test
+    @DisplayName("payEmployees should return 0 for unsuccessful payments (Mockito)")
+    void payEmployeesShouldReturn0ForUnsuccessfulPayments() {
+        BankService bankServiceMock = Mockito.mock(BankService.class);
+        doThrow(RuntimeException.class).when(bankServiceMock).pay(anyString(), anyDouble());
+
+        EmployeeRepository employeeRepository = new EmployeeRepositoryStub();
+        Employees employees = new Employees(employeeRepository, bankServiceMock);
+
+        int payments = employees.payEmployees();
+
+        // Verifiera att antalet lyckade betalningar är 0 eftersom RuntimeException kastas för varje betalning
+        assertThat(payments).isEqualTo(0);
+        // Verifiera att metoden anropats
+        verify(bankServiceMock, times(3)).pay(anyString(), anyDouble());
+        // kontrollera isPaid() är falskt för varje employee i findAll()
+        assertThat(employeeRepository.findAll()).extracting(Employee::isPaid).containsOnly(false);
+    }
 }
